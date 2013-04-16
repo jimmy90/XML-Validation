b35e832566c74b1c8babef625dc680bf22915ec9
jimmy90 <jimmy@jimmy-Presario-CQ56-Notebook-PC.(none)>
2013-04-16 02:45:29 +0200

    Edition des structures des documents xml et dtd + exemple

--- Projet.ml
+++ Projet.ml
@@ -8,7 +8,19 @@ type balises= OUVRANTE of id
 type documentXML= DOCUMENTXML of (entry list)
 and entry= DATA of string
 	   |ENTRY of balises*(entry list);;
-
+(*
+<contacts>
+ <prenom> Jimmy </prenom>
+<ville> olivet </ville>
+ </contact>
+*)
+let essai= DOCUMENTXML [
+ENTRY (OUVRANTE "contacts",[
+ENTRY (OUVRANTE "prenom",[DATA "Jimmy"]);
+ENTRY (FERMANTE "prenom",[DATA ""]);
+ENTRY (OUVRANTE "ville",[DATA "olivet"]);
+ENTRY (FERMANTE "olivet",[DATA ""])] );
+ENTRY (FERMANTE "contacts",[DATA ""])];;
 (* Structure de la DTD *)
 
 type elements= ELEMENTS of occurence*atom
